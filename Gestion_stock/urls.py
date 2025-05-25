98582aac88bc165c834cc4c1d578e9beb5676cc5
MohamedMBG <baghdadmohamed.me@gmail.com>
2025-05-24 22:58:31 -0400

    Fix: Add missing URL name 'admin_clients' to resolve NoReverseMatch

--- Gestion_stock/urls.py
+++ Gestion_stock/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     re_path(r'^admindash/$', TemplateView.as_view(template_name='frontoffice/master_page.html')),
     re_path(r'^admindash/statistiques$', TemplateView.as_view(template_name='frontoffice/page/statistiques.html')),
     re_path(r'^admindash/produits$', TemplateView.as_view(template_name='frontoffice/page/produit.html'), name='admin_produits'),
-    re_path(r'^admindash/clients$', TemplateView.as_view(template_name='frontoffice/page/client.html')),
+    re_path(r'^admindash/clients$', TemplateView.as_view(template_name='frontoffice/page/client.html'), name='admin_clients'),
     re_path(r'^admindash/fournisseurs$', TemplateView.as_view(template_name='frontoffice/page/fournisseur.html')),
     re_path(r'^admindash/achats$', TemplateView.as_view(template_name='frontoffice/page/achat.html')),
     # Client Portal URLs
